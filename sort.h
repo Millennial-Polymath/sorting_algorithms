e30c638046fc5ebb0dade4cededef3dc76315ae6
Millennial-Polymath <muchamingotho@gmail.com>
2021-06-15 10:32:51 +0000

    updated sort.h

--- sort.h
+++ sort.h
@@ -21,4 +21,8 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void sorted_insert(listint_t **list, listint_t *newNode);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+
 #endif /* SORT_H */

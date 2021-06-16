6172f888ba2a6ef109767a865ec7c8182ef5e123
Millennial-Polymath <muchamingotho@gmail.com>
2021-06-16 05:59:56 +0000

    Add insertion sort alternative algorthm

---
+++ 1-insertion_out_of_place.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+/**
+ * sorted_insert - inserts a new node in a sorted list
+ * @list: the sorted list
+ * @newNode: the node from the unsorted list to be inserted in the
+ * the new sorted list
+ */
+void sorted_insert(listint_t **list, listint_t *newNode)
+{
+	
